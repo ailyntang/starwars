dd03cc473a59383450c16f9ccf2edd9fa4ed50da
ailyntang <ailyntang@gmail.com>
2019-10-02 19:43:03 +1000

    [TECH-001] Add nav title to home screen

--- StarWars/Home/HomeViewController.swift
+++ StarWars/Home/HomeViewController.swift
@@ -9,6 +9,7 @@ final class HomeViewController: UIViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    title = "Star Wars"
     tableView.register(UINib.init(nibName: cellIdentifier, bundle: nil), forCellReuseIdentifier: cellIdentifier)
   }
 }
