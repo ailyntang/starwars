e148fbf2f7c11d8f63336220cf396f67131aab12
ailyntang <ailyntang@gmail.com>
2019-10-02 16:35:06 +1000

    [TECH-001] Configure `HomeCell` using a category

--- StarWars/Home/HomeCell.swift
+++ StarWars/Home/HomeCell.swift
@@ -5,7 +5,8 @@ final class HomeCell: UITableViewCell {
   @IBOutlet private var mainImageView: UIImageView!
   @IBOutlet private var mainLabel: UILabel!
   
-  func configure(with text: String) {
-    mainLabel.text = text
+  func configure(with category: Category) {
+    mainImageView.image = category.icon
+    mainLabel.text = category.title
   }
 }
