5bd14f612e2d8e18edf4b8fafcfe7030858e4854
ailyntang <ailyntang@gmail.com>
2019-10-03 21:53:01 +1000

    [TECH-002] Create function to fetch films from API

--- StarWars/Films/FilmsViewModel.swift
+++ StarWars/Films/FilmsViewModel.swift
@@ -5,10 +5,23 @@ final class FilmsViewModel {
   private var title: String
   private var director: String
   private var openingCrawl: String
-  
+
   init(with film: Film){
     self.title = film.title
     self.director = film.director
     self.openingCrawl = film.openingCrawl
+  }
+  
+  func loadFilms() {
+    let networkManager = NetworkManager()
+    networkManager.fetchFilms() { [weak self] results, errorMessage in
+      if let results = results {
+        print("YAAAY results! \n\(results)")
+      }
+      
+      if !errorMessage.isEmpty {
+        print("Error loading films: \(errorMessage)")
+      }
+    }
   }
 }
