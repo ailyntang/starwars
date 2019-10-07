f59b282fb81a9b4918b96279350943f06ad0f184
ailyntang <ailyntang@gmail.com>
2019-10-08 10:44:31 +1100

    [TECH-004] Remove description label from `FilmCell`

--- StarWars/Films/FilmCell.swift
+++ StarWars/Films/FilmCell.swift
@@ -4,11 +4,9 @@ import UIKit
 final class FilmCell: UITableViewCell {
   @IBOutlet private var titleLabel: UILabel!
   @IBOutlet private var subtitleLabel: UILabel!
-  @IBOutlet private var descriptionLabel: UILabel!
 
   func configure(with film: Film) {
     titleLabel.text = film.title
     subtitleLabel.text = String(film.episodeId)
-    descriptionLabel.text = "film.openingCrawl"
   }
 }
