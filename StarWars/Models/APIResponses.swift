bccd05412881b94c287d733feef80b2a64e1229d
ailyntang <ailyntang@gmail.com>
2019-10-08 11:04:02 +1100

    [TECH-005] Move `Film` struct into `APIResponses` file

--- StarWars/Models/APIResponses.swift
+++ StarWars/Models/APIResponses.swift
@@ -5,3 +5,19 @@ struct FilmSummary: Codable {
   let count: Int?
   let results: [Film]?
 }
+
+struct Film: Codable {
+  let title: String
+  let episodeId: Int
+  
+  enum CodingKeys: String, CodingKey {
+    case title
+    case episodeId = "episode_id"
+  }
+  
+  init(title: String,
+       episodeId: Int) {
+    self.title = title
+    self.episodeId = episodeId
+  }
+}
