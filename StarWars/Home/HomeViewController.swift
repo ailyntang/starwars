48dfba1a90eb2cfff2562113e4d3955bf8d29940
ailyntang <ailyntang@gmail.com>
2019-10-17 11:17:32 +1100

    [TECH-006] Move logic in `HomeViewController` to `HomeViewModel`

--- StarWars/Home/HomeViewController.swift
+++ StarWars/Home/HomeViewController.swift
@@ -4,26 +4,26 @@ import UIKit
 final class HomeViewController: UIViewController {
   @IBOutlet private var tableView: UITableView!
   
+  private let viewModel = HomeViewModel()
   private let cellIdentifier = "HomeCell"
-  private let categories: [Category] = [.films, .people, .planets, .species, .starships, .vehicles]
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    title = "Star Wars"
+    title = viewModel.title
     tableView.register(UINib.init(nibName: cellIdentifier, bundle: nil), forCellReuseIdentifier: cellIdentifier)
   }
 }
 
 extension HomeViewController: UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return categories.count
+    return viewModel.numberOfRowsIn(section)
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? HomeCell else {
       fatalError("Issue with dequeuing \(cellIdentifier)")
     }
-    cell.configure(with: categories[indexPath.row])
+    cell.configure(with: viewModel.cellForRowAt(indexPath))
     return cell
   }
 }
@@ -31,15 +31,6 @@ extension HomeViewController: UITableViewDataSource {
 extension HomeViewController: UITableViewDelegate {
   func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     tableView.deselectRow(at: indexPath, animated: true)
-    
-    let category = categories[indexPath.row]
-    let viewController: UIViewController
-    
-    switch category {
-    case .films: viewController = FilmsViewController()
-    default: viewController = FilmsViewController()
-    }
-    
-    navigationController?.pushViewController(viewController, animated: true)
+    navigationController?.pushViewController(viewModel.viewControllerAt(indexPath), animated: true)
   }
 }
--- StarWars/Home/HomeViewModel.swift
+++ StarWars/Home/HomeViewModel.swift
@@ -1,2 +1,26 @@
 
-import Foundation
+import UIKit
+
+struct HomeViewModel {
+  private let categories: [Category] = [.films, .people, .planets, .species, .starships, .vehicles]
+  let title = "Star Wars"
+  
+  func numberOfRowsIn(_ section: Int) -> Int {
+    return section == 0 ? categories.count : 0
+  }
+  
+  func cellForRowAt(_ indexPath: IndexPath) -> Category {
+    return categories[indexPath.row]
+  }
+  
+  func viewControllerAt(_ indexPath: IndexPath) -> UIViewController {
+    let viewController: UIViewController
+    
+    switch categories[indexPath.row] {
+    case .films: viewController = FilmsViewController()
+    default: viewController = FilmsViewController()
+    }
+    
+    return viewController
+  }
+}
